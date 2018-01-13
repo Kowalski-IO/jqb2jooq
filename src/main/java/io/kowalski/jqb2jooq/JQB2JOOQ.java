17a9e8f724fba917d50191f5244b5d57acae8034
Brandon Kowalski <brandon@kowalski.io>
2018-01-12 23:42:52 -0500

    Javadocs + Central Deploy pom changes
    Just needs some tests

--- src/main/java/io/kowalski/jqb2jooq/JQB2JOOQ.java
+++ src/main/java/io/kowalski/jqb2jooq/JQB2JOOQ.java
@@ -4,8 +4,18 @@ import org.jooq.Condition;
 
 import java.util.Map;
 
+/**
+ * JQB2JOOQ is the entry point of the library and handles the conversion
+ * of a Json Filter to a JOOQ Condition
+ */
 public class JQB2JOOQ {
 
+    /**
+     * parse performs the transformation of filter to condition.
+     * @param targetClass must be an Enum that implements {@link RuleTarget}
+     * @param jsonFilter is the raw Json String representation of the jQuery QueryBuilder filter deserialized into a Map
+     * @return the JOOQ Condition equivalent of the provided Json filter
+     */
     public static Condition parse(final Class<? extends RuleTarget> targetClass, final Map<String, Object> jsonFilter) {
         Filter filter = FilterParser.parseJSON(targetClass, jsonFilter);
         return FilterTranslator.translate(filter);
--- src/main/java/io/kowalski/jqb2jooq/RuleTarget.java
+++ src/main/java/io/kowalski/jqb2jooq/RuleTarget.java
@@ -5,19 +5,26 @@ import org.jooq.Field;
 
 /**
  * This represents a template for an Enum to be defined against your model.
- *
+ * <p>
  * The constants of your Enum are to mirror the ids of the values within
  * your jQuery QueryBuilder configuration. These will map to a JOOQ Field (preferably
  * provided by auto-generated code). You then may provide an optional var-args collection
  * of Implicit Conditions which will automatically be applied to the user supplied condition.
- *
+ * <p>
  * i.e. if a table within your schema holds a variety of types (perhaps made distinct by some discriminator column)
  * you can add in implicit conditions that will be automatically applied to the aforementioned condition (via an AND).
  */
 public interface RuleTarget {
 
+    // Getter Contract
     Field getField();
     Condition[] getImplicitConditions();
+
+    /**
+     * parse is accessed via reflection and will call your Enum's valueOf method
+     * @param value the rule id as it appears in jQuery QueryBuilder
+     * @return the parsed RuleTarget as defined by your Enumeration definition
+     */
     RuleTarget parse(String value);
 
 }
